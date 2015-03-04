rboulton/es_dump_restore

commit dbc9f931447fd86f6ad2fa7977ca92b340f42e9c

Make sure that _search/scroll request path does not include index
It turns out that while you call the _search endpoint you embed
index and type name in the URI path, but when requesting the
_search/scroll endpoint it should be directly below the elasticsearch
base uri. This change also introduces error reporting if a http
request fails.

lib/es_dump_restore/es_client.rb
@@ -11,19 +11,20 @@ module EsDumpRestore
       @httpclient = HTTPClient.new
       @index_name = index_name
 
-      @base_uri = type.nil? ? URI.parse(base_uri + "/" + index_name + "/") : URI.parse(base_uri + "/" + index_name + "/" + type + "/")
+      @es_uri = base_uri
+      @path_prefix = type.nil? ? index_name : index_name + "/" + type
     end
 
     def mappings
-      request(:get, '_mapping')[index_name]
+      request(:get, "#{@path_prefix}/_mapping")[index_name]
     end
 
     def settings
-      request(:get, '_settings')[index_name]
+      request(:get, "#{@path_prefix}/_settings")[index_name]
     end
 
     def start_scan(&block)
-      scroll = request(:get, '_search',
+      scroll = request(:get, "#{@path_prefix}/_search",
         query: { search_type: 'scan', scroll: '10m', size: 500 },
         body: MultiJson.dump({ query: { match_all: {} } }) )
       total = scroll["hits"]["total"]
@@ -34,9 +35,9 @@ module EsDumpRestore
 
     def each_scroll_hit(scroll_id, &block)
       loop do
-        batch = request(:get, '_search/scroll', query: {
+        batch = request(:get, '_search/scroll', {query: {
           scroll: '10m', scroll_id: scroll_id
-        })
+        }}, [404])
 
         batch_hits = batch["hits"]
         break if batch_hits.nil?
@@ -59,10 +60,19 @@ module EsDumpRestore
 
     private
 
-    def request(method, path, options={})
-      request_uri = @base_uri + path
-      response = @httpclient.request(method, request_uri, options)
-      MultiJson.load(response.content)
+    def request(method, path, options={}, extra_allowed_exitcodes=[])
+      request_uri = @es_uri + "/" + path
+      begin
+        response = @httpclient.request(method, request_uri, options)
+        unless response.ok? or extra_allowed_exitcodes.include? response.status
+          raise "Request failed with status #{response.status}: #{response.reason}"
+        end
+        MultiJson.load(response.content)
+      rescue Exception => e
+        puts "Exception caught issuing HTTP request to #{request_uri}"
+        puts "options: #{options}"
+        raise e
+      end
     end
   end
 end

lib/es_dump_restore/app.rb
@@ -8,6 +8,7 @@ module EsDumpRestore
   class App < Thor
     
     option :noprogressbar, :type => :boolean # switch for showing progress bar
+    option :verbose, :type => :boolean # add some additional output
 
     desc "dump URL INDEX_NAME FILENAME", "Creates a dumpfile based on the given ElasticSearch index"
     def dump(url, index_name, filename)

commit 2a06193045d04aa3cc4f8431e3ee7909bd71c3d7

Remove unicode no-break-space.
The first space in this string was actually "\u00A0" (NO-BREAK-SPACE)[1]
(introduced in dbc9f931).

This was causing load errors on older versions of ruby eg:

```
/usr/lib/ruby/1.9.1/rubygems/custom_require.rb:36:in `require':
/var/lib/gems/1.9.1/gems/es_dump_restore-0.0.10/lib/es_dump_restore/es_client.rb:68:
invalid multibyte char (US-ASCII) (SyntaxError)
/var/lib/gems/1.9.1/gems/es_dump_restore-0.0.10/lib/es_dump_restore/es_client.rb:68:
invalid multibyte char (US-ASCII)
/var/lib/gems/1.9.1/gems/es_dump_restore-0.0.10/lib/es_dump_restore/es_client.rb:68:
syntax error, unexpected $end, expecting keyword_end
          raise "Request failed with status #{response...
```

[1] http://www.fileformat.info/info/unicode/char/00a0/index.htm

lib/es_dump_restore/es_client.rb
@@ -65,7 +65,7 @@ module EsDumpRestore
       begin
         response = @httpclient.request(method, request_uri, options)
         unless response.ok? or extra_allowed_exitcodes.include? response.status
-          raise "Request failed with status #{response.status}: #{response.reason}"
+          raise "Request failed with status #{response.status}: #{response.reason}"
         end
         MultiJson.load(response.content)
       rescue Exception => e